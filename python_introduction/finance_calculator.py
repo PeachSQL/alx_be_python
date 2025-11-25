d5512e4f6bb7a5f8a2ad202893c62b3346a213cc
PeachSQL <peachsql@gmail.com>
2025-11-25 12:25:06 +0300

    Adding finance calculator script

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,10 @@
 #A finance calculator to enable user to calculate their savings over a one month period
-monthly_income_string = input("Enter your monthly income: ")
-monhtly_income = int(monthly_income_string)
+monthly_income= input("Enter your monthly income: ")
 
-monthly_expenses_string = input("Enter your monhtly expenses: ")
-monthly_expenses = int(monthly_expenses_string)
 
-monthly_savings = (monhtly_income - monthly_expenses)
+monthly_expenses= input("Enter your monhtly expenses: ")
+
+monthly_savings = (float(monthly_income) - float(monthly_expenses))
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
