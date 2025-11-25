0f0ce7bd883d37c2feefff83a0d2a8542f7c3161
PeachSQL <peachsql@gmail.com>
2025-11-25 11:59:07 +0300

    Adding future age calculator script

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,5 @@
+#A python script to take user age input and calculate their age in a future year
+age_string = input("How old are you? ")
+age_number = int(age_string)
+future_age = age_number + 27
+print("In 2050, you will be", future_age, "years old.")
