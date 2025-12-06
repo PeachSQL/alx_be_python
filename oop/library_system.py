cdc6dcd4d2ee188e5d8af41f1b0f07c8228de5f1
PeachSQL <peachsql@gmail.com>
2025-12-06 13:38:12 +0300

    Mastering Inheritance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,28 +1,37 @@
 class Book:
-    def __init__(self, title, author):
+    def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
 
-    def details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class EBook(Book):
-    def __init__(self, title, author, file_size):
+    def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class PrintBook(Book):
-    def __init__(self, title, author, page_count):
+    def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
+
+    def __repr__(self):
+        return self.__str__()
 
 
 class Library:
@@ -34,4 +43,4 @@ class Library:
 
     def list_books(self):
         for b in self.books:
-            print(b.details())
+            print(str(b))
